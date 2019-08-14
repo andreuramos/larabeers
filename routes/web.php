44c459fb3284e2ec14aa49e7efb38a7cd954681f
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-14 11:30:24 +0200

    generated auth controllers & views

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,19 @@
 */
 
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
+Route::get('/create_admin', function () {
     $admin_user = User::first();
 
     if (!$admin_user) {
-        $admin_user = new User(['name' => 'admin', 'email'=> 'andreu.ramos.amengual@gmail.com', 'password' => '123']);
+        $admin_user = new User(['name' => 'admin', 'email'=> 'andreu.ramos.amengual@gmail.com', 'password' => Hash::make('123')]);
         $admin_user->save();
     }
 
     return $admin_user->email;
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
