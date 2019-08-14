f890d3576d9af97dfc80e145e695215025547ef8
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-14 17:09:53 +0200

    set timestamps to false

--- app/Label.php
+++ app/Label.php
@@ -8,6 +8,8 @@ class Label extends Model
 {
     protected $fillable = ["beer_id", "year", "month", "album", "page", "position"];
 
+    public $timestamps = false;
+
     public function tags()
     {
         return $this->belongsToMany('App\Tag');
