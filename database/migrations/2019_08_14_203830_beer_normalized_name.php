b96bc11f0b807aa41871f4a02b33484b6e835a39
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-15 10:01:00 +0200

    created normalized name for beer search

---
+++ database/migrations/2019_08_14_203830_beer_normalized_name.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Beer;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeerNormalizedName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('beers', function (Blueprint $t) {
+            $t->string('normalized_name')->after('name')->nullable();
+        });
+
+        foreach (Beer::all() as $beer) {
+            $beer->normalized_name = App\Helpers\StringHelper::normalize($beer->name);
+            $beer->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('beers', function (Blueprint $t) {
+            $t->dropColumn('normalized_name');
+        });
+    }
+}
