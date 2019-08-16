6cf2b6e84e7f1bb6d355055d3c47465d13660ec1
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-16 14:13:27 +0200

    added ext-json

---
+++ resources/views/frontend/beer/beer.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>{{ $beer->name }}</h1>
+
+
+        <div class="carrousel slide" data-ride="carrousel">
+
+        </div>
+    </div>
+
+
+@endsection
