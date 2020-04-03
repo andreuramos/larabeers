c955478202d5f56738155b396411027af6b55632
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-04-03 15:33:50 +0200

    added label template to the list

--- resources/views/frontend/beer_item.blade.php
+++ resources/views/frontend/beer_item.blade.php
@@ -1,8 +1,11 @@
 <div class="col-12 beer-list__beer">
-    <div class="beer-list__beer__name">
-        <a href="{{ url('/beer/'.$beer->id) }}">{{ $beer->name }}</a>
+    <div class="beer-list__beer__image">
+        <img src="{{ URL::asset('img/label-template.jpg') }}"/>
     </div>
     <div class="beer-list__beer__data">
+        <div class="beer-list__beer__name">
+            <a href="{{ url('/beer/'.$beer->id) }}">{{ $beer->name }}</a>
+        </div>
         <span class="beer-list__beer__data__brewer">{{ $beer->brewers[0]->name }}</span>
         <div class="badge badge-secondary">{{ $beer->labels[0]->year }}</div>
     </div>
