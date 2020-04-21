b5c884d447456826c08a6050fc72309bd89e0957
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-04-21 19:36:07 +0200

    update brewer service

--- Larabeers/Services/Tests/UpdateBrewerTest.php
+++ Larabeers/Services/Tests/UpdateBrewerTest.php
@@ -5,6 +5,7 @@ namespace Larabeers\Services\Tests;
 use Larabeers\Entities\Brewer;
 use Larabeers\Entities\City;
 use Larabeers\Entities\Country;
+use Larabeers\Entities\Image;
 use Larabeers\External\BrewerRepository;
 use Larabeers\External\Images\Uploader\ImageUploader;
 use Larabeers\Services\UpdateBrewer;
@@ -42,7 +43,7 @@ class UpdateBrewerTest extends ServiceTestBase
             ->shouldBeCalled()
             ->willReturn(null);
 
-        $this->getService()->execute(0, "-",$this->city);
+        $this->getService()->execute(0, "-", $this->city);
     }
 
     /**
@@ -50,7 +51,7 @@ class UpdateBrewerTest extends ServiceTestBase
      */
     public function test_empty_name()
     {
-        $this->getService()->execute(1, "" , $this->city);
+        $this->getService()->execute(1, "", $this->city);
     }
 
     public function test_object_without_data()
@@ -61,7 +62,7 @@ class UpdateBrewerTest extends ServiceTestBase
         $brewer->city = $this->city;
 
         $new_city = new City("Brewville", new Country("Brewlandia"));
-        
+
         $new_brewer = new Brewer();
         $new_brewer->name = "new name";
         $new_brewer->normalized_name = "new name";
@@ -81,7 +82,47 @@ class UpdateBrewerTest extends ServiceTestBase
 
     public function test_object_with_data()
     {
+        $brewer = new Brewer();
+        $brewer->name = $this->name;
+        $brewer->normalized_name = $this->normalized_name;
+        $brewer->city = $this->city;
 
+        $new_city = new City("Brewville", new Country("Brewlandia"));
+
+        $updated_brewer = new Brewer();
+        $updated_brewer->name = "new name";
+        $updated_brewer->normalized_name = "new name";
+        $updated_brewer->city = $new_city;
+        $updated_brewer->address = "Brew Ave";
+        $updated_brewer->latitude = 2.15321;
+        $updated_brewer->longitude = 0.525435;
+        $updated_brewer->website = "www.brewery.com";
+        $logo = new Image();
+        $logo->url = "cloud.cdn.org/file.jpg";
+        $updated_brewer->logo = $logo;
+
+        $data = [
+            'address' => "Brew Ave",
+            'lat' => 2.15321,
+            'lng' => 0.525435,
+            'website' => "www.brewery.com",
+            'logo' => "path/to/file.jpg"
+        ];
+
+        $this->brewer_repository->findById(1)
+            ->shouldBeCalled()
+            ->willReturn($brewer);
+        $this->normalize_string->execute("new name")
+            ->shouldBeCalled()
+            ->willReturn("new name");
+        $this->image_uploader->upload("path/to/file.jpg")
+            ->shouldBeCalled()
+            ->willReturn("cloud.cdn.org/file.jpg");
+        $this->brewer_repository->save($updated_brewer)
+            ->shouldBeCalled()
+            ->willReturn(1);
+
+        $this->getService()->execute(1, "new name", $new_city, $data);
     }
 
     public function getService()
--- Larabeers/Services/UpdateBrewer.php
+++ Larabeers/Services/UpdateBrewer.php
@@ -2,7 +2,9 @@
 
 namespace Larabeers\Services;
 
+use Larabeers\Entities\Brewer;
 use Larabeers\Entities\City;
+use Larabeers\Entities\Image;
 use Larabeers\Exceptions\BrewerNotFoundException;
 use Larabeers\Exceptions\ServiceArgumentException;
 use Larabeers\External\BrewerRepository;
@@ -35,5 +37,48 @@ class UpdateBrewer
         if (!$brewer) {
             throw new BrewerNotFoundException("brewer $id does not exist");
         }
+
+        $normalized_name = $this->normalize_string->execute($name);
+
+        $brewer->name = $name;
+        $brewer->normalized_name = $normalized_name;
+        $brewer->city = $city;
+
+        if ($data !== null) {
+            $brewer = $this->set_brewer_data($brewer, $data);
+        }
+
+        $this->brewer_repository->save($brewer);
+    }
+
+    private function set_brewer_data(Brewer $brewer, array $data): Brewer
+    {
+        if (array_key_exists('address', $data)) {
+            $brewer->address = $data['address'];
+        }
+
+        if (array_key_exists('lat', $data) && array_key_exists('lng', $data)) {
+            $brewer->latitude = $data['lat'];
+            $brewer->longitude = $data['lng'];
+        }
+
+        if (array_key_exists('website', $data)) {
+            $brewer->website = $data['website'];
+        }
+
+        if (array_key_exists('logo', $data)) {
+            $logo = $this->create_logo($data['logo']);
+            $brewer->logo = $logo;
+        }
+
+        return $brewer;
+    }
+
+    private function create_logo($logo): Image
+    {
+        $url = $this->image_uploader->upload($logo);
+        $logo = new Image();
+        $logo->url = $url;
+        return $logo;
     }
 }
