8de8c4757e8ad061dda498befe806d9fef619588
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-05-06 19:01:09 +0200

    dummy random api method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Larabeers\External\BeerRepository;
+use Larabeers\External\FlagRepository;
 
 class ApiController extends Controller
 {
     private $beer_repository;
+    private $flag_repository;
 
     public function __construct(
-        BeerRepository $beer_repository
+        BeerRepository $beer_repository,
+        FlagRepository $flag_repository
     ) {
         $this->beer_repository = $beer_repository;
+        $this->flag_repository = $flag_repository;
     }
 
     public function randomBeers(Request $request) {
@@ -25,11 +29,18 @@ class ApiController extends Controller
                 $image = $sticker->thumbnail ?? $sticker->url;
             }
 
+            $year = null;
+            if (count($beer->labels)) {
+                $year = $beer->labels[0]->year;
+            }
+
             $beers[] = [
                 'id' => $beer->id,
                 'name' => $beer->name,
                 'brewer' => $beer->brewers[0]->name,
-                'thumbnail' => $image
+                'thumbnail' => $image,
+                'flag' => $this->flag_repository->get($beer->brewers[0]->city->country->name),
+                'year' => $year
             ];
         }
         return response()->json($beers);
