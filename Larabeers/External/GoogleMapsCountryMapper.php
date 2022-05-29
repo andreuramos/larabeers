6d369810a6876889c3b48761938f6ae92e466feb
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-05-29 13:00:33 +0200

    refactor - map in array instead of ifs

--- Larabeers/External/GoogleMapsCountryMapper.php
+++ Larabeers/External/GoogleMapsCountryMapper.php
@@ -6,15 +6,18 @@ use Larabeers\Domain\Location\CountryMapper;
 
 class GoogleMapsCountryMapper implements CountryMapper
 {
+    private const MAPPED_COUNTRIES = [
+        "USA" => "United States",
+        "England" => "United Kingdom",
+        "Slovak Republic" => "Slovakia",
+    ];
+
     public function execute(string $country_name): string
     {
-        if ($country_name === "USA") {
-            $country_name = "United States";
-        } elseif ($country_name === "England") {
-            $country_name = "United Kingdom";
-        } elseif ($country_name === "Slovak Republic") {
-            $country_name = "Slovakia";
+        if (array_key_exists($country_name, self::MAPPED_COUNTRIES)) {
+            return self::MAPPED_COUNTRIES[$country_name];
         }
+
         return $country_name;
     }
 }
